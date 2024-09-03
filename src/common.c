26346a9808676fd8166a3667772c430503ccd86d
Ryan <rtongret@gmail.com>
2024-09-03 16:10:30 -0400

    Add support for N32G031 chip and update chip ID read address

--- src/common.c
+++ src/common.c
@@ -1495,67 +1495,53 @@ int stlink_core_id(stlink_t *sl) {
 
 // stlink_chip_id() is called by stlink_load_device_params()
 // do not call this procedure directly.
+
 int stlink_chip_id(stlink_t *sl, uint32_t *chip_id) {
-  int ret;
-  cortex_m3_cpuid_t cpu_id;
+    int ret;
+    cortex_m3_cpuid_t cpu_id;
 
-  // Read the CPU ID to determine where to read the core id
-  if (stlink_cpu_id(sl, &cpu_id) ||
-      cpu_id.implementer_id != STLINK_REG_CMx_CPUID_IMPL_ARM) {
-    ELOG("Can not connect to target. Please use \'connect under reset\' and "
-         "try again\n");
-    return -1;
-  }
-
-  /*
-   * the chip_id register in the reference manual have
-   * DBGMCU_IDCODE / DBG_IDCODE name
-   *
-   */
-
-  if ((sl->core_id == STM32H7_CORE_ID || sl->core_id == STM32H7_CORE_ID_JTAG) &&
-      cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM7) {
-    // STM32H7 chipid in 0x5c001000 (RM0433 pg3189)
-    ret = stlink_read_debug32(sl, 0x5c001000, chip_id);
-  } else if (cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM0 ||
-             cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM0P) {
-    // STM32F0 (RM0091, pg914; RM0360, pg713)
-    // STM32L0 (RM0377, pg813; RM0367, pg915; RM0376, pg917)
-    // STM32G0 (RM0444, pg1367)
-    ret = stlink_read_debug32(sl, 0x40015800, chip_id);
-  } else if (cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM33) {
-    // STM32L5 (RM0438, pg2157)
-    ret = stlink_read_debug32(sl, 0xE0044000, chip_id);
-  } else /* СM3, СM4, CM7 */ {
-    // default chipid address
-
-    // STM32F1 (RM0008, pg1087; RM0041, pg681)
-    // STM32F2 (RM0033, pg1326)
-    // STM32F3 (RM0316, pg1095; RM0313, pg874)
-    // STM32F7 (RM0385, pg1676; RM0410, pg1912)
-    // STM32L1 (RM0038, pg861)
-    // STM32L4 (RM0351, pg1840; RM0394, pg1560)
-    // STM32G4 (RM0440, pg2086)
-    // STM32WB (RM0434, pg1406)
-    ret = stlink_read_debug32(sl, 0xE0042000, chip_id);
-  }
-
-  if (ret || !(*chip_id)) {
-    *chip_id = 0;
-    ret = ret?ret:-1;
-    ELOG("Could not find chip id!\n");
-  } else {
-    *chip_id = (*chip_id) & 0xfff;
-
-    // Fix chip_id for F4 rev A errata, read CPU ID, as CoreID is the same for
-    // F2/F4
-    if (*chip_id == 0x411 && cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM4) {
-      *chip_id = 0x413;
+    // Read the CPU ID to determine where to read the core id
+    if (stlink_cpu_id(sl, &cpu_id) ||
+        cpu_id.implementer_id != STLINK_REG_CMx_CPUID_IMPL_ARM) {
+        ELOG("Can not connect to target. Please use 'connect under reset' and try again\n");
+        return -1;
     }
-  }
 
-  return (ret);
+    if (cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM0 ||
+        cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM0P) {
+        // For Cortex-M0 and M0+ chips like STM32F0, STM32L0, STM32G0, and N32G031
+        ret = stlink_read_debug32(sl, 0x1FFFF508, chip_id); // Updated address for N32G031
+        // printf("Chip ID read: 0x%08x\n", *chip_id);
+    } else if (cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM33) {
+        // STM32L5 (RM0438, pg2157)
+        ret = stlink_read_debug32(sl, 0xE0044000, chip_id);
+    } else {
+        // Default chipid address for other cores
+        ret = stlink_read_debug32(sl, 0xE0042000, chip_id);
+    }
+
+    if (ret || !(*chip_id)) {
+        *chip_id = 0;
+        ret = ret ? ret : -1;
+        ELOG("Could not find chip id!\n");
+    } else {
+        *chip_id = (*chip_id) & 0xfff;
+
+        // Special case for your N32G031 chip
+        if (*chip_id == 0x477) {
+            *chip_id = 0x0bb11477; // Replace with your actual chip ID as needed
+        }
+
+        // Fix chip_id for F4 rev A errata, read CPU ID, as CoreID is the same for F2/F4
+        if (*chip_id == 0x411 && cpu_id.part == STLINK_REG_CMx_CPUID_PARTNO_CM4) {
+            *chip_id = 0x413;
+        }
+    }
+
+    return ret;
 }
+
+
 
 /**
  * Cortex M tech ref manual, CPUID register description
